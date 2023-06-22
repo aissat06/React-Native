475c90dc56dc2f76aa149b9f4ecd0cb0d0450015
AISSAT Youcef <youcefaissat7@gmail.com>
2023-06-22 14:07:45 +0200

    Créer une application react de liste des tâches à ajouter/compléter

--- App.js
+++ App.js
@@ -1,20 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+
+  const addTask = () => {
+    if (taskText.trim() !== '') {
+      setTasks([...tasks, { id: Date.now(), text: taskText }]);
+      setTaskText('');
+    }
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const toggleTaskCompletion = (taskId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const renderTask = ({ item }) => (
+    console.log("taskes...", item),
+    <View style={styles.taskContainer}>
+      <View>
+        <Text style={styles.taskText}>{item.text}</Text>
+      </View>
+      <View style={styles.taskContainer}>
+        <TouchableOpacity
+          style={styles.deleteButton}
+          onPress={() => deleteTask(item.id)}
+        >
+          <Text style={styles.deleteButtonText}>Supprimer</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.completeButton}
+          onPress={() => toggleTaskCompletion(item.id)}
+        >
+        <Text style={styles.completeButtonText}>
+          {item.completed ? 'Annuler' : 'Terminé'}
+        </Text>
+        </TouchableOpacity>
+      </View>
+    </View> 
+  );
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Liste des tâches</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Ajouter une tâche"
+          value={taskText}
+          onChangeText={setTaskText}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={addTask}>
+          <Text style={styles.addButtonText}>Ajouter</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={tasks}
+        renderItem={renderTask}
+        keyExtractor={(item) => item.id.toString()}
+        ListEmptyComponent={() => (
+          <Text style={styles.emptyText}>Aucune tâche pour le moment</Text>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 16,
+    justifyContent: 'flex-start',
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    marginTop: 8,
+    textAlign : "center",
+  },
+  taskContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
+    display:'flex',
+    justifyContent  : 'space-between',
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  taskText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  deleteButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    backgroundColor: '#FF0000',
+    borderRadius: 4,
+  },
+  deleteButtonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  completeButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    backgroundColor: '#4CAF50',
+    borderRadius: 4,
+  },
+  completeButtonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    marginTop: 8,
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    marginRight: 8,
+    borderWidth: 1,
+    padding: 8,
+  },
+  addButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    backgroundColor: '#007AFF',
+    borderRadius: 4,
+  },
+  addButtonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  emptyText: {
+    textAlign: 'center',
+    fontStyle: 'italic',
+    marginTop: 20,
+    color: 'red',
   },
 });
